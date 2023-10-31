289ad28575014d7212d93b3c084e51790636b5c6
찬호 <dev.lukej@gmail.com>
2023-10-31 19:09:33 +0900

    aws 3tier architecture / app

--- src/main/java/com/example/aws3tierarchitecture/Controller/UserController.java
+++ src/main/java/com/example/aws3tierarchitecture/Controller/UserController.java
@@ -11,15 +11,11 @@ import java.util.List;
 
 @Controller
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
     @GetMapping("/")
     public String dispMain() {
 
-        return "index";
+        return "index.html";
     }
 
     @GetMapping("/signup")
